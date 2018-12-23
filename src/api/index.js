2cc36b1ccb36aa2bc71c73d02a38a7ccd24623f2
KasenLi <kasenkelvin.10@gmail.com>
2018-12-23 15:37:41 -0400

    HTTPS

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import config from './config'
 
 //const apiKey = config.apiKey
 const { apiKey } = config
-const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json&limit=:limit`
+const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=${apiKey}&format=json&limit=:limit`
 
 export default function getArtists(country, limit) {
 	const url = URL.replace(':country', country)
